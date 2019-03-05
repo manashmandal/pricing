162c5ae726bff7eedc7a1d3d48913b6bf72c0c4c
Manash <manashmndl@gmail.com>
2019-03-05 18:54:17 +0600

    Updates

---
+++ precache-manifest.9b944810091a6abd02652672eb3a349e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d28fc6975f4248ebd87",
+    "url": "/pricing/static/css/main.dc584bd2.chunk.css"
+  },
+  {
+    "revision": "9d28fc6975f4248ebd87",
+    "url": "/pricing/static/js/main.9d28fc69.chunk.js"
+  },
+  {
+    "revision": "45276941fbd92a6cff10",
+    "url": "/pricing/static/js/runtime~main.45276941.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/pricing/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pricing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c2e14930b8dcc3b014aaf6d69fe1a0d1",
+    "url": "/pricing/index.html"
+  }
+];
